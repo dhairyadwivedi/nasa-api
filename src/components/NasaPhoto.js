55fd94199ad5d38fccc38b569f0e495a010042e0
dhairyadwivedi <dhairiya96@gmail.com>
2020-09-13 00:02:41 +0530

    fix text and image scaling issue

--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -4,9 +4,7 @@ export const NasaPhoto = () => {
   const [photoData, setPhotoData] = useState({});
 
   async function fetchPhoto(date) {
-    const res = await fetch(
-      `https://api.nasa.gov/planetary/apod?api_key=Zh7WE3pY03LRHUSR4KB0HdjRXPHwwqDKO1GRgVt4`
-    );
+    const res = await fetch(process.env.REACT_APP_KEY);
     const data = await res.json();
     setPhotoData(data);
   }
@@ -17,10 +15,11 @@ export const NasaPhoto = () => {
 
   return (
     <div className="container">
-      <img className="nasa-image" src={photoData.hdurl} alt="nasa" />
       <div>
         <h1>{photoData.title}</h1>
         <span>{photoData.date}</span>
+
+        <img className="nasa-image" src={photoData.hdurl} alt="nasa" />
         <p>{photoData.explanation}</p>
       </div>
     </div>
